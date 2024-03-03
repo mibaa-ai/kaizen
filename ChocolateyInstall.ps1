bf0f8ab505322298087a0e67c577f7ab2f7a56e9
mibaa-ai <56147179+mibaa-ai@users.noreply.github.com>
2024-03-03 20:41:15 +0300

    Update ChocolateyInstall.ps1
    proba

--- ChocolateyInstall.ps1
+++ ChocolateyInstall.ps1
@@ -2,7 +2,7 @@
 # This is where you see the top level API - with xml to Packages - should look nearly the same as https://community.chocolatey.org/api/v2/
 # If you are using Nexus, always add the trailing slash or it won't work
 # === EDIT HERE ===
-$packageRepo = 'https://github.com/mibaa-ai/kaizen.git'
+$packageRepo = 'https://github.com/mibaa-ai/kaizen'
 
 # If the above $packageRepo repository requires authentication, add the username and password here. Otherwise these leave these as empty strings.
 $repoUsername = ''    # this must be empty is NOT using authentication
